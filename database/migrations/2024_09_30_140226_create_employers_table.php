dffaae98259ead8602637ec0f6a771ae82919cbd
April Denise Bihag <adbihag1@gmail.com>
2024-09-30 22:24:41 +0800

    0914_12-Pivot Tables and BelongsToMany Relationships

--- database/migrations/2024_09_30_140226_create_employers_table.php
+++ database/migrations/2024_09_30_140226_create_employers_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
     /**
@@ -11,21 +9,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('job_listings', function (Blueprint $table) {
+        Schema::create('employers', function (Blueprint $table) {
             $table->id();
-//            $table->unsignedBigInteger('employer_id');
-            $table->foreignIdFor(\App\Models\Employer::class);
-            $table->string('title');
-            $table->string('salary');
+            $table->string('name');
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_listings');
+        Schema::dropIfExists('employers');
     }
 };
