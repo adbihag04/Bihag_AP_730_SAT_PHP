0f3eab5c3d8a9d0ccdc50dc2b1f4abb160fc8c6c
April Denise Bihag <adbihag1@gmail.com>
2024-10-01 23:53:08 +0800

    0928_23-6-steps-to-authorization-mastery

--- database/factories/EmployerFactory.php
+++ database/factories/EmployerFactory.php
@@ -1,6 +1,10 @@
 <?php
+
 namespace Database\Factories;
+
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employer>
  */
@@ -14,7 +18,8 @@ class EmployerFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->company()
+            'name' => fake()->company(),
+            'user_id' => User::factory()
         ];
     }
 }
--- database/migrations/2024_09_30_140226_create_employers_table.php
+++ database/migrations/2024_09_30_140226_create_employers_table.php
@@ -1,7 +1,9 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 return new class extends Migration
 {
     /**
@@ -11,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('employers', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(\App\Models\User::class);
             $table->string('name');
             $table->timestamps();
         });
     }
+
     /**
      * Reverse the migrations.
      */
