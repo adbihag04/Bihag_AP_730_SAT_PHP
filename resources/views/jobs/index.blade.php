32171dcc484a6d8d3ca863cec7a1206655c899cc
April Denise Bihag <adbihag1@gmail.com>
2024-10-04 20:51:32 +0800

    1001_26-get-your-frontend-build-process-in-order

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -9,10 +9,11 @@
                 <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
 
                 <div>
-                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+                    <strong class="text-laracasts">{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
                 </div>
             </a>
         @endforeach
+
         <div>
             {{ $jobs->links() }}
         </div>
